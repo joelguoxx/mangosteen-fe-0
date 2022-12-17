7eee90ebe3051b12b662024f44c70829ce627eca
joelguoxx <2936146778@qq.com>
2022-12-17 11:12:09 +0800

    css初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import { defineComponent } from "vue";
+import { RouterView } from "vue-router";
+import './App.scss'
+
 export const App = defineComponent({
   setup() { 
-    return () => <>
-      <header>导航
-        <ul>
-          <li>
-            <router-link to='/'>Foo</router-link>
-          </li>
-          <li>
-            <router-link to='/about'>Bar</router-link>
-          </li>
-        </ul>
-      </header>
-      <div>
-        <router-view></router-view>
+    return () => (
+       <div>
+        <RouterView></RouterView>
       </div>
-    </>
+    )
   }
 })
