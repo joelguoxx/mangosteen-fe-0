18a58f972d19cbcfd0c63e59f25c8334f05cfeb3
joelguoxx <2936146778@qq.com>
2022-12-20 11:48:24 +0800

    重构： WelcomeLayout 组件

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,22 +1,23 @@
 import { defineComponent } from 'vue';
-import s from './First.module.scss'
+import s from './WelcomeLayout.module.scss'
 import cloud from '../../assets/icons/cloud.svg'
 import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout';
 
 export const Forth = defineComponent({
 setup: (props, context) => {
 return () => (
-  <div class={s.wrapper}>
-    <div class={s.card}>
-      <img class={s.pig}  src={cloud} />
-    <h2>云备份<br></br>再也不怕数据丢失</h2>
-    </div>
-    <div class={s.actions}>
+  <WelcomeLayout>
+    {{
+      icon: () => <img src={cloud} />,
+      title: () => <h2>云备份<br></br>再也不怕数据丢失</h2>,
+      buttons: () => <>
       <RouterLink class={s.fake} to=''>跳过</RouterLink>
-      <RouterLink to='/start'>完成</RouterLink>
-      <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
-    </div>
-</div>
+       <RouterLink to='/start'>完成</RouterLink>
+       <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
+    </>
+    }}
+</WelcomeLayout>
 )
 }
 })
