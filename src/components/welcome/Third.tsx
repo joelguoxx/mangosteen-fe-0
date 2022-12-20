50b983e4173016bfcc7d0857127c395d419a012e
joelguoxx <2936146778@qq.com>
2022-12-20 13:03:21 +0800

    解决了页面有滚动条的Bug

--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -5,19 +5,19 @@ import { RouterLink } from 'vue-router';
 import { WelcomeLayout } from './WelcomeLayout';
 
 export const Third = defineComponent({
-setup: (props, context) => {
-return () => (
-  <WelcomeLayout>
-  {{
-    icon: () => <img src={chart} />,
-    title: () => <h2>数据可视化<br />收支一目了然</h2>,
-    buttons: () => <>
-      <RouterLink class={s.fake} to=''>跳过</RouterLink>
-    <RouterLink to='/welcome/4'>下一页</RouterLink>
-    <RouterLink to='/start'>跳过</RouterLink>
-  </>
-  }}
-</WelcomeLayout>
-)
-}
+  setup: (props, context) => {
+    return () => (
+      <WelcomeLayout>
+        {{
+          icon: () => <img src={chart} />,
+          title: () => <h2>数据可视化<br />收支一目了然</h2>,
+          buttons: () => <>
+            <RouterLink class={s.fake} to=''>跳过</RouterLink>
+            <RouterLink to='/welcome/4'>下一页</RouterLink>
+            <RouterLink to='/start'>跳过</RouterLink>
+          </>
+        }}
+      </WelcomeLayout>
+    )
+  }
 })
--- src/components/welcome/WelcomeLayout.tsx
+++ src/components/welcome/WelcomeLayout.tsx
@@ -3,17 +3,17 @@ import s from './WelcomeLayout.module.scss'
 
 export const WelcomeLayout = defineComponent({
   setup: (props, context) => {
-    const { slots } = context
-return () => (
-  <div class={s.wrapper}>
-    <div class={s.card}>
-      {slots.icon?.()}
-      {slots.title?.()}
-    </div>
-    <div class={s.actions} >
-      {slots.buttons?.()}
-    </div>
- </div>
-)
-}
+    const { slots: { icon, title, buttons } } = context
+    return () => (
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          {icon?.()}
+          {title?.()}
+        </div>
+        <div class={s.actions} >
+          {buttons?.()}
+        </div>
+      </div>
+    )
+  }
 })
