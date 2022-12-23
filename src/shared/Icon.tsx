c6123302c4db4672e922be830f6b28deff744abb
joelguoxx <2936146778@qq.com>
2022-12-23 23:19:06 +0800

    修复了iconName可以为空的bug

--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,12 +1,13 @@
 import { defineComponent, PropType } from 'vue';
 import s from './Icon.module.scss';
 
-
+export type IconName = 'add' | 'chart' | 'clock' | 'mangsteen' | 'pig'
 
 export const Icon = defineComponent({
   props: {
     name: {
-      type: String as PropType<'add' | 'chart' | 'clock' | 'mangsteen' | 'pig'>
+      type: String as PropType<IconName>,
+      required: true
     }
   },
   setup: (props, context) => {
