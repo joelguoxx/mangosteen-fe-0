0cb8017f0e325b217e839ff924c9b12f72fd29dc
joelguoxx <2936146778@qq.com>
2023-01-14 13:04:58 +0800

    打算使用grid改写数字键盘

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,6 +1,9 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import { Icon } from '../../shared/Icon';
 import s from './InputPad.module.scss';
+import { time } from '../../shared/time'
+import { DatePicker, Popup } from 'vant';
+import 'vant/lib/index.css';
 export const InputPad = defineComponent({
   props: {
     name: {
@@ -8,31 +11,52 @@ export const InputPad = defineComponent({
     }
   },
   setup: (props, context) => {
+    const now = new Date()
+    const refDate = ref<Date>(now)
+    const minDate = ref(new Date(2021, 0, 1))
+    const maxDate = ref(new Date(2025, 5, 1))
+    const appendText = (n: number | string) => refAmount.value += n.toString()
     const buttons = [
-      { text: '1', onClick: () => { } },
-      { text: '2', onClick: () => { } },
-      { text: '3', onClick: () => { } },
-      { text: '删除', onClick: () => { } },
-      { text: '4', onClick: () => { } },
-      { text: '5', onClick: () => { } },
-      { text: '6', onClick: () => { } },
-      { text: '+', onClick: () => { } },
-      { text: '7', onClick: () => { } },
-      { text: '8', onClick: () => { } },
-      { text: '9', onClick: () => { } },
-      { text: '-', onClick: () => { } },
-      { text: '.', onClick: () => { } },
-      { text: '0', onClick: () => { } },
-      { text: '清空', onClick: () => { } },
+      {
+        text: '1', onClick: () => { appendText(1) }
+      },
+      { text: '2', onClick: () => { appendText(2) } },
+      { text: '3', onClick: () => { appendText(3) } },
+      { text: '4', onClick: () => { appendText(4) } },
+      { text: '5', onClick: () => { appendText(5) } },
+      { text: '6', onClick: () => { appendText(6) } },
+      { text: '7', onClick: () => { appendText(7) } },
+      { text: '8', onClick: () => { appendText(8) } },
+      { text: '9', onClick: () => { appendText(9) } },
+      { text: '.', onClick: () => { appendText('.') } },
+      { text: '0', onClick: () => { appendText(0) } },
+      { text: '删除', onClick: () => { refAmount.value = refAmount.value.substring(0, refAmount.value.length - 1) } },
       { text: '提交', onClick: () => { } },
     ]
+    const refDatePickerVisble = ref(false)
+    const showDatePicker = () => refDatePickerVisble.value = true
+    const hideDatePicker = () => refDatePickerVisble.value = false
+    const setDate = (date: Date) => { refDate.value = date; hideDatePicker() }
+    const refAmount = ref('')
     return () => <>
       <div class={s.dateAndamount}>
         <span class={s.date}>
           <Icon name='date' class={s.icon} />
-          <span>2023-01-01</span>
+          <span>
+            <span onClick={showDatePicker} >{time(refDate.value).format()}</span>
+            <Popup position='bottom' v-model:show={refDatePickerVisble.value} >
+              <DatePicker
+                v-model={refDate.value}
+                title="选择日期"
+                min-date={minDate.value}
+                max-date={maxDate.value}
+                onConfirm={hideDatePicker}
+                onCancel={hideDatePicker}
+              />
+            </Popup>
+          </span>
         </span>
-        <span class={s.amount}>1999.614</span>
+        <span class={s.amount}>{refAmount.value}</span>
       </div>
       <div class={s.buttons}>
         {buttons.map(button => <button onClick={button.onClick}>{button.text}</button>)}
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { svgstore } from './src/vite_plugins/svgstore';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/mangosteen-fe-0/dist/',
@@ -12,5 +14,8 @@ export default defineConfig({
       mergeProps: true
     }),
     svgstore(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
   ]
 })
