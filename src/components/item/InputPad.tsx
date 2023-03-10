eca843ce0c92f7517dcbd75be6607fe96474cfa6
joelguoxx <2936146778@qq.com>
2023-03-10 20:48:06 +0800

    提交收集四个字段 kind、tag_id、amount 和 happen_at

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -5,35 +5,37 @@ import { Time } from '../../shared/time'
 import { DatetimePicker, NumberKeyboard, Popup } from 'vant';
 export const InputPad = defineComponent({
   props: {
-    name: {
+    happenAt: {
       type: String as PropType<string>
-    }
+    },
+    amount: Number
   },
   setup: (props, context) => {
-    const now = new Date()
-    const refDate = ref<Date>(now)
     const minDate = ref(new Date(2020, 0, 1))
     const maxDate = ref(new Date(2025, 11, 31))
     const appendText = (n: number | string) => {
       const nString = n.toString()
       const dotIndex = refAmount.value.indexOf('.')
-      if (nString === '.') {
-        if (dotIndex >= 0) {
-          return
-        } else if (refAmount.value === '') {
-          refAmount.value = '0'
-        }
-      }
-      if (refAmount.value === '0') {
-        if (nString != '.') {
-          return
-        }
+      if (refAmount.value.length >= 13) {
+        return
       }
       if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) {
         return
       }
-      if (refAmount.value.length >= 13) {
-        return
+      if (nString === '.') {
+        if (dotIndex >= 0) { // 已经有小数点了
+          return
+        }
+      } else if (nString === '0') {
+        if (dotIndex === -1) { // 没有小数点
+          if (refAmount.value === '0') { // 没小数点，但是有0
+            return
+          }
+        }
+      } else {
+        if (refAmount.value === '0') {
+          refAmount.value = ''
+        }
       }
       refAmount.value += n.toString()
     }
@@ -53,23 +55,23 @@ export const InputPad = defineComponent({
       { text: '.', onClick: () => { appendText('.') } },
       { text: '0', onClick: () => { appendText(0) } },
       { text: '删除', onClick: () => { refAmount.value = refAmount.value.substring(0, refAmount.value.length - 1) } },
-      { text: '清空', onClick: () => { refAmount.value = '' } },
-      { text: '提交', onClick: () => { } },
+      { text: '清空', onClick: () => { refAmount.value = '0' } },
+      { text: '提交', onClick: () => context.emit('update:amount', parseFloat(refAmount.value) * 100) },
     ]
     const refDatePickerVisble = ref(false)
     const showDatePicker = () => refDatePickerVisble.value = true
     const hideDatePicker = () => refDatePickerVisble.value = false
-    const setDate = (date: Date) => { refDate.value = date; hideDatePicker() }
-    const refAmount = ref('')
+    const setDate = (date: Date) => { context.emit('update:happenAt', date.toISOString()); hideDatePicker() }
+    const refAmount = ref(props.amount ? (props.amount / 100).toString() : '0')
     return () => <>
       <div class={s.dateAndamount}>
         <span class={s.date}>
           <Icon name='date' class={s.icon} />
           <span>
-            <span onClick={showDatePicker}>{new Time(refDate.value).format()}</span>
+            <span onClick={showDatePicker}>{new Time(props.happenAt).format()}</span>
             <Popup position='bottom' v-model:show={refDatePickerVisble.value}>
               <DatetimePicker
-                value={refDate.value}
+                value={props.happenAt}
                 type="date"
                 title="选择年月日"
                 minDate={minDate.value}
